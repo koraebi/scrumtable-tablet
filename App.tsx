5ea662790e8f178f361b5df2e7e3be359551df7f
Nadaelabdellaoui20 <nada.el-abdellaoui@etu.univ-cotedazur.fr>
2022-01-28 10:52:59 +0100

    ihm tablet

--- App.tsx
+++ App.tsx
@@ -11,8 +11,21 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Scrumtable</Text>
-      <Button title="Message" onPress={() => socket.emit('message', 'Touch')} />
+      <View style={styles.RectangleShapeViewIssues}>
+        <Text>ALL THE ISSUES</Text>
+      </View>
+      <View style={styles.RectangleShapeViewIssue}>
+        <Text>SELECTED ISSUE</Text>
+        <Button title="Issue" onPress={() => socket.emit('message', 'Touch')} />
+      </View>
+      <View style={styles.matrix}>
+        <View style={styles.must} />
+        <View style={styles.should} />
+      </View>
+      <View style={styles.matrix}>
+        <View style={styles.could} />
+        <View style={styles.wont} />
+      </View>
     </View>
   );
 }
@@ -20,8 +33,55 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#99A3A4',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+
+  RectangleShapeViewIssues: {
+    marginTop: 20,
+    width: 560,
+    height: 120,
+    padding: 10,
+    backgroundColor: '#CD7DDD',
+  },
+  RectangleShapeViewIssue: {
+    marginTop: 20,
+    width: 560,
+    height: 120,
+    padding: 10,
+    backgroundColor: '#CD7DDD',
+  },
+  matrix: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+    padding: 10,
+  },
+  must: {
+    backgroundColor: '#78D17B',
+    flex: 2,
+    marginRight: 5,
+    padding: 10,
+  },
+  should: {
+    backgroundColor: '#7FA9E9',
+    flex: 2,
+    marginLeft: 5,
+    padding: 10,
+  },
+  could: {
+    backgroundColor: '#E7DA6E',
+    flex: 2,
+    marginLeft: 5,
+    padding: 10,
+  },
+  wont: {
+    backgroundColor: '#E86868',
+    flex: 2,
+    marginLeft: 5,
+    padding: 10,
   },
 });
