f1926b01d4e73923317590d27b8ba703052e050d
Nadaelabdellaoui20 <nada.el-abdellaoui@etu.univ-cotedazur.fr>
2022-02-10 21:49:40 +0100

    fix getting informations from api

--- api/nestjsapi.js
+++ api/nestjsapi.js
@@ -1,8 +1,8 @@
-import { Moscow } from '../src/enum/moscow';
+import {Moscow} from '../src/enum/moscow';
 import {Issue} from '../src/model/Issue';
 
-export function getIssuesFromApi () {
-  return fetch('http://10.188.111.29:3000/issues')
+export function getIssuesFromApi() {
+  return fetch('http://192.168.1.81:3000/issues')
     .then(response => response.json())
     .catch(error => {
       console.error(error);
@@ -16,17 +16,64 @@ export function parseIssuesInfo(json) {
   let shouldData = [];
   let couldData = [];
   let wontData = [];
+  //this.name = name;
+  //this.description = description;
+  //this.number = number;
+  //this.selected = selected;
+  //this.moscow = Moscow.moscow;
   for (let i = 0; i < json.length; i++) {
-    if(json[i].moscow === undefined) 
-      availableData.push(new Issue(json[i].name, json[i].selected));
-    else if(json[i].moscow === Moscow.MUST)
-      mustData.push(new Issue(json[i].name, json[i].selected));
-    else if(json[i].moscow === Moscow.SHOULD)
-      shouldData.push(new Issue(json[i].name, json[i].selected));
-    else if(json[i].moscow === Moscow.COULD)
-      couldData.push(new Issue(json[i].name, json[i].selected));
-    else if(json[i].moscow === Moscow.WONT)
-      wontData.push(new Issue(json[i].name, json[i].selected));
+    if (json[i].moscow === undefined) {
+      availableData.push(
+        new Issue(
+          json[i].name,
+          json[i].description,
+          json[i].number,
+          json[i].selected,
+          json[i].moscow,
+        ),
+      );
+    } else if (json[i].moscow === Moscow.MUST) {
+      mustData.push(
+        new Issue(
+          json[i].name,
+          json[i].description,
+          json[i].number,
+          json[i].selected,
+          json[i].moscow,
+        ),
+      );
+    } else if (json[i].moscow === Moscow.SHOULD) {
+      shouldData.push(
+        new Issue(
+          json[i].name,
+          json[i].description,
+          json[i].number,
+          json[i].selected,
+          json[i].moscow,
+        ),
+      );
+    } else if (json[i].moscow === Moscow.COULD) {
+      couldData.push(
+        new Issue(
+          json[i].name,
+          json[i].description,
+          json[i].number,
+          json[i].selected,
+          json[i].moscow,
+        ),
+      );
+    } else if (json[i].moscow === Moscow.WONT) {
+      wontData.push(
+        new Issue(
+          json[i].name,
+          json[i].description,
+          json[i].number,
+          json[i].selected,
+          json[i].moscow,
+        ),
+      );
+    }
   }
-  return data = [availableData, mustData, shouldData, couldData, wontData];
+  data = [availableData, mustData, shouldData, couldData, wontData];
+  return data;
 }
