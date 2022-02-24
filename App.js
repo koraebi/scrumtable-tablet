e5e27316e1d168b8b15ea5e7405b30878e6e8d3c
Sabri <sabri.habib@etu.unice.fr>
2022-02-24 12:57:45 +0100

    crash fix

--- App.js
+++ App.js
@@ -1,271 +1,157 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
-import { Alert, StyleSheet, View, Text, Modal, ScrollView, Image, Pressable } from 'react-native';
+import { StyleSheet, View, Text, Modal, ScrollView, Image, Pressable, Alert } from 'react-native';
 import { SOCKET } from '@env'
-import { getIssuesFromApi, parseIssuesInfo, removeLabelToIssue, addLabelToIssue } from './src/services/api';
+import { getIssuesFromApi, parseIssuesInfo, removeLabels, addLabel } from './src/services/api';
 import { Board, BoardRepository } from 'react-native-draganddrop-board'
+import { Button } from 'react-native-elements/dist/buttons/Button';
 
-let issues = [];
-export default class App extends Component {
-
-  socket = io.connect(SOCKET, { 
+export default function App()  {
+  const socket = io.connect(SOCKET, { 
     transports: ['websocket'],
     forceNew: true,
     reconnectionAttempts: 100,
   });
+  socket.on('updateTabletIssue', (issue) => this.updateIssue(issue));
+  socket.on('lockTabletIssue', (issueNumber) => this.lockIssue(issueNumber, true));
+  socket.on('unlockTabletIssue', (issueNumber) => this.lockIssue(issueNumber), false);
 
-  labels = ['MUST', 'SHOULD', 'COULD', 'WONT', 'X'];
+  let boardRepository = new BoardRepository([]); 
 
-  state = {
-    boardRepository: new BoardRepository([]),
-    selectedIssue: {},
-    showModal: false
-  };
+  const [lastMovedIssue, setLastMovedIssue] = useState({});
+  const [boardData, setBoardData] = useState([
+    {
+      id: 1, 
+      name: 'Must',  
+      rows: []
+    }, 
+    {
+      id: 2,
+      name: 'Should',
+      rows: []
+    },
+    { 
+      id: 3,
+      name: 'Could',
+      rows: []
+    }, 
+    {
+      id: 4,
+      name: "Won't", 
+      rows: []
+    }, 
+    {
+      id: 5,
+      name: "ToDos",
+      rows: []
+    }
+  ]);
 
-  render() {
-    return (
-      <View>
-        <Board
-          boardRepository={this.state.boardRepository}
-          open={(issue) => this.onIssueTouched(issue)}
-          onDragEnd={(srcColumn, destColumn, draggedItem) => this.onIssueDragEnd(srcColumn, destColumn, draggedItem)}
-          boardBackground='black'
-          cardContent={(issue) => (
-            <View style={[styles.card, {borderColor: issue.selectionColor}]}>
-              <View style={styles.row}>
-                <Text style={styles.textCardId}>{issue.id}</Text>
-                <Text numberOfLines={1} style={[styles.textCard, {color: issue.selectionColor}]}>{issue.name}</Text>
-              </View>
-              <Text numberOfLines={1} style={{fontSize: 20, color: "gray", marginTop: 5}}>{issue.description}</Text>
-            </View>
-          )}
-        />
-        <Modal
-          style={styles.modal}
-          animationType="slide"
-          transparent={false}
-          visible={this.state.showModal}
-          onRequestClose={() => {
-            this.setState({
-              showModal: false
-            });
-          }}
-        >
-          <ScrollView style={styles.scrollview}>
-            <View style={styles.row}>
-              <Text style={[styles.text, {fontSize: 30, marginRight: 5}]}>{this.state.selectedIssue.id}</Text>
+  useEffect(() => {
+    const fetchIssues = async () => {
+      const json = await getIssuesFromApi();
+      const issues = await parseIssuesInfo(json);
+      boardData[0].rows = issues[0];
+      boardData[1].rows = issues[1];
+      boardData[2].rows = issues[2];
+      boardData[3].rows = issues[3];
+      boardData[4].rows = issues[4];
+      setBoardData(boardData);
+    }
+    fetchIssues();
+  }, []);
 
-              <View style={[styles.label, {display: this.state.selectedIssue.displayLabel, right:1, position:'absolute', alignSelf: 'center', borderColor: this.state.selectedIssue.textColor, backgroundColor: this.state.selectedIssue.backgroundColor}]}>
-                <Text style={[styles.textLabel, {color: this.state.selectedIssue.textColor}]}>{this.state.selectedIssue.label}</Text>
-              </View>
-            </View>
-            
-            <Text style={[styles.text, styles.textTitle]}>{this.state.selectedIssue.name}</Text>
+  useEffect(() => {
+    boardRepository.updateData(boardData);
+  }, [boardData]);
 
-            <View style={[styles.row, {display: this.state.selectedIssue.displayAssignee, marginVertical:10}]}>
-              <Image 
-                source={{uri: this.state.selectedIssue.assigneeAvatarUrl}}  
-                style={{width: 50, height: 50, borderRadius: 50/ 2}} 
-              />
-              <Text style={[styles.text, styles.textAssignee]}>{this.state.selectedIssue.assigneeName}</Text>
-            </View>
-            
-            <Text style={[styles.text, styles.textDescription]}>{this.state.selectedIssue.description}</Text>
-          </ScrollView>
-            
-          <Pressable
-            style={styles.button}
-            onPress={() => this.setState({
-              showModal: false
-            })}
-          >
-            <Text style={styles.textButton}>FERMER</Text>
-          </Pressable>
-        </Modal>
-      </View>
-    );
-  }
-
-  async componentDidMount() {
-    this.updateIssues("");
-
-    this.socket.on('updateTabletIssues', (message) => this.updateIssues(message));
-    this.socket.on('lockTabletIssue', (message) => this.lockIssue(message, true));
-    this.socket.on('unlockTabletIssue', (message) => this.lockIssue(message), false);
-  }
-
-  async updateIssues(message) {
-    const json = await getIssuesFromApi();
-    issues = parseIssuesInfo(json);
-    this.loadBoard();
-  }
-
-  async lockIssue(issueNumber, lock) {
-    console.log(issueNumber + ' is locked');
+  updateIssue = (issue) => {
+    if (issue.number === lastMovedIssue.number) return;
 
     for (let i = 0; i <= 4; i++) {
-      const index = issues[i].findIndex(issue => issue.number.toString() === issueNumber);
-      console.log(index);
+      const index = boardData[i].rows.findIndex(issue => issue.number.toString() === issueNumber);
       if (index !== -1) {
-        console.log('FOUND');
-        const issue = issues[i][index];
-        issue.selectionColor = lock ? "red" : "#4734D3";
-        issues[i][index] = issue;
-        this.loadBoard();
+        boardData[i].rows[index].label = issue.label;
+        setBoardData(boardData);
+        return;
+      } 
+    } 
+  }
+
+  lockIssue = (issueNumber, lock) => {
+    for (let i = 0; i <= 4; i++) {
+      const index = boardData[i].rows.findIndex(issue => issue.number.toString() === issueNumber);
+      if (index !== -1) {
+        boardData[i].rows[index].selectionColor = lock ? "red" : "#4734D3";
+        setBoardData(boardData);
+        return;
       } 
     } 
   }
   
-  async onIssueDragEnd(srcColumn, destColumn, draggedItem) {
+  onIssueDragEnd = async (srcColumn, destColumn, draggedItem) => {
     const issue = draggedItem.attributes.row;
-    const newLabel = this.labels[destColumn - 1];
+    const newLabel = boardData[destColumn - 1].name;
 
-    if (issue.label.toLowerCase().replace("'", '') === newLabel.toLowerCase()) return;
+    if ((issue.label === newLabel) || (issue.label === '' && newLabel === 'ToDos')) return;
 
-    if (issue.label !== 'X') {
-      await removeLabelToIssue(issue);
+    if (newLabel === "ToDos") {
+      await removeLabels(issue);
+    } else {
+      await addLabel(issue, newLabel);
+    } 
+  }
+
+  onIssueTouched = (issue) => {
+    let description = issue.description ?? "";
+    if (issue.assigneeName) {
+      description += "\n\nAssigné à : " + issue.assigneeName
     }
 
-    await addLabelToIssue(issue, newLabel);
+    Alert.alert(
+      issue.id + "  " + issue.name,
+      description,
+    )
+  } 
 
-    this.socket.emit('updateWebIssues', issue.id); 
-  }
-
-  async onIssueTouched(issue) {
-    this.socket.emit('lockWebIssue', issue.id);
-    this.setState({
-      selectedIssue: issue,
-      showModal: true
-    });
-  }
-
-  loadBoard() {
-    const boardData = [
-      {
-        id: 1, 
-        name: 'MUST',
-        rows: issues[1]
-      },
-      {
-        id: 2,
-        name: 'SHOULD',
-        rows: issues[2]
-      },
-      { 
-        id: 3,
-        name: 'COULD',
-        rows: issues[3]
-      },
-      {
-        id: 4,
-        name: "WON'T",
-        rows: issues[4]
-      }, 
-      {
-        id: 5,
-        name: "TODO",
-        rows: issues[0]
-      }
-    ];
-
-    this.setState({
-      boardRepository: new BoardRepository(boardData)
-    });
-  }
+  return (
+    <Board
+      boardRepository={boardRepository}
+      open={(issue) => onIssueTouched(issue)}
+      onDragEnd={(srcColumn, destColumn, draggedItem) => onIssueDragEnd(srcColumn, destColumn, draggedItem)}
+      boardBackground='black'
+      cardContent={(issue) => (
+        <View style={{
+          borderColor: issue.selectionColor,
+          marginRight: 20,
+          fontSize: 20,
+          fontWeight: '400',
+          padding: 10,
+          borderRadius: 15,
+          backgroundColor: 'white',
+          textAlign: "left",
+          color: "#4734D3",
+          margin: 10,
+          borderWidth: 0.5}}
+        >
+          <View style={{flexDirection:'row'}}>
+            <Text style={{
+              marginRight: 5,
+              alignSelf: 'center',
+              color: "#8B949E",
+              fontWeight: '300',
+              fontSize: 20}}
+            >{issue.id}</Text>
+            <Text numberOfLines={1} style={{
+              marginRight: 20,
+              fontWeight: '400',
+              fontSize: 20,
+              color: issue.selectionColor}}
+            >{issue.name}</Text>
+          </View>
+          <Text numberOfLines={1} style={{fontSize: 20, color: "gray", marginTop: 5}}>{issue.description}</Text>
+        </View>
+      )}
+    />
+  )
 }
-
-const styles = StyleSheet.create({
-  row: {
-    flexDirection:'row'
-  },
-  card: {
-    fontWeight: '400',
-    fontSize: 20,
-    padding: 10,
-    borderRadius: 15,
-    backgroundColor: 'white',
-    textAlign: "left",
-    color: "#4734D3",
-    margin: 10,
-    borderWidth: 0.5
-  },
-  text: {
-    textAlign: 'left',
-  },
-  textAssignee: {
-    marginLeft: 5,
-    alignSelf: 'center',
-    textAlign: 'left',
-    fontStyle: 'italic',
-    fontSize: 20,
-    color: 'grey',
-    fontWeight: '300'
-  },
-  textCard: {
-    marginRight: 20,
-    fontWeight: '400',
-    fontSize: 20,
-  },
-  textCardId: {
-    marginRight: 5,
-    alignSelf: 'center',
-    color: "#8B949E",
-    fontWeight: '300',
-    fontSize: 20,
-  },
-  textTitle: {
-    color: "#4734D3",
-    fontWeight: '400',
-    fontSize: 30,
-  },
-  textDescription: {
-    marginBottom: 100,
-    marginTop: 10,
-    fontWeight: '300',
-    fontSize: 20,
-    color: 'black'
-  },
-  textButton: {
-    marginHorizontal: 20,
-    textAlign: "center",
-    color: 'white'
-  },
-  textLabel: {
-    color: 'white',
-    flexShrink: 1
-  },
-  label: {
-    marginVertical: 5,
-    alignSelf: 'flex-start',
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    elevation: 2,
-    textAlign: "center",
-    borderRadius: 20,
-    borderWidth: 1
-  },
-  scrollview: {
-    marginHorizontal: 30,
-    marginTop: 30
-  },
-  button: {
-    position: 'absolute',
-    alignSelf: 'center',
-    bottom: 1,
-    backgroundColor: "#4734D3",
-    borderRadius: 30,
-    padding: 15,
-    elevation: 2,
-    marginHorizontal: 50,
-    marginBottom: 30
-  },
-  modal: {
-    flexDirection:'row',
-    flex: 1,
-    backgroundColor: "white",
-    borderRadius: 20,
-    alignItems: "center",
-    shadowRadius: 4,
-    elevation: 5
-  },
-})
--- src/services/api.js
+++ src/services/api.js
@@ -1,98 +1,116 @@
 import { Issue } from '../models/issue';
-import { API } from '@env'
+import { API, GITHUB_PAT } from '@env'
 
 export async function getIssuesFromApi() {
   try {
-    const response = await fetch(API + '/');
+    const response = await fetch('https://api.github.com/repos/Scrumtable/web/issues', { 
+      method: 'GET', 
+      headers: new Headers({
+        "Authorization": 'Bearer ' + GITHUB_PAT,
+        'Content-Type': 'application/json'
+      })
+    });
+    
     return response.json();
   } catch(e) {
-    console.error('Error from getIssuesFromApi() => ' + e);
+    console.error('Error from getIssuesFromApi() => ' + e); 
   }
 }
 
-export async function addLabelToIssue(issue, newLabel) {
+export async function addLabel(issue, newLabel) {
   try {
-    const response = await fetch([API, issue.number, newLabel].join('/'), { method: 'POST' });
+    const response = await fetch('https://api.github.com/repos/Scrumtable/web/issues/' + issue.number + '/labels', { 
+      method: 'PUT', 
+      body: JSON.stringify({labels: [newLabel]}),
+      headers: new Headers({
+        "Authorization": 'Bearer ' + GITHUB_PAT,
+        'Content-Type': 'application/json'
+      })
+    });
+
     return response.json();
   } catch(e) {
-    console.error('Error from addLabelToIssue() => ' + e);
+    console.error('Error from addLabel() => ' + e);
   }
 }
 
-export async function removeLabelToIssue(issue) {
+export async function removeLabels(issue) {
   try {
-    const response = await fetch([API, issue.number, issue.label].join('/'), { method: 'DELETE' });
+    const response = await fetch('https://api.github.com/repos/Scrumtable/web/issues/' + issue.number + '/labels/' + issue.label, { 
+      method: 'DELETE', 
+      headers: new Headers({
+        "Authorization": 'Bearer ' + GITHUB_PAT,
+        'Content-Type': 'application/json'
+      })
+    });
+
     return response.json();
   } catch(e) {
-    console.error('Error from removeLabelToIssue() => ' + e);
+    console.error('Error from removeLabels() => ' + e);
   }
 }
 
-export function parseIssuesInfo(json) {
-  let availableData = [];
-  let mustData = [];
-  let shouldData = [];
-  let couldData = [];
-  let wontData = [];
+export async function parseIssuesInfo(json) { 
+  let mustIssues = [];
+  let shouldIssues = [];
+  let couldIssues = [];
+  let wontIssues = [];
+  let todoIssues = [];
   
   for (let i = 0; i < json.length; i++) {
-    if (json[i].moscow === undefined) {
-      availableData.push(
+    if (json[i].labels.length) {
+      if (json[i].labels[0].name === 'Must' && mustIssues.length < 5) {
+        mustIssues.push(
+          new Issue(
+            json[i].title,
+            json[i].body,
+            json[i].number,
+            'Must',
+            json[i].assignee
+          )
+        );
+      } else if (json[i].labels[0].name === 'Should' && shouldIssues.length < 5) {
+        shouldIssues.push(
+          new Issue(
+            json[i].title,
+            json[i].body,
+            json[i].number,
+            'Should',
+            json[i].assignee
+          )
+        );
+      } else if (json[i].labels[0].name === 'Could' && couldIssues.length < 5) {
+        couldIssues.push(
+          new Issue(
+            json[i].title,
+            json[i].body,
+            json[i].number,
+            'Should',
+            json[i].assignee
+          )
+        );
+      } else if (json[i].labels[0].name === "Won't" && wontIssues.length < 5) {
+        wontIssues.push(
+          new Issue(
+            json[i].title,
+            json[i].body,
+            json[i].number,
+            "Won't",
+            json[i].assignee
+          )
+        );
+      }
+    } else if (todoIssues.length < 5) {
+      todoIssues.push(
         new Issue(
-          json[i].name,
-          json[i].description,
+          json[i].title,
+          json[i].body,
           json[i].number,
-          json[i].selected,
-          json[i].moscow,
-          json[i].assignee,
-        ),
-      );
-    } else if (json[i].moscow === 'Must') {
-      mustData.push(
-        new Issue(
-          json[i].name,
-          json[i].description,
-          json[i].number,
-          json[i].selected,
-          json[i].moscow,
-          json[i].assignee,
-        ),
-      );
-    } else if (json[i].moscow === 'Should') {
-      shouldData.push(
-        new Issue(
-          json[i].name,
-          json[i].description,
-          json[i].number,
-          json[i].selected,
-          json[i].moscow,
-          json[i].assignee,
-        ),
-      );
-    } else if (json[i].moscow === 'Could') {
-      couldData.push(
-        new Issue(
-          json[i].name,
-          json[i].description,
-          json[i].number,
-          json[i].selected,
-          json[i].moscow,
-          json[i].assignee,
-        ),
-      );
-    } else if (json[i].moscow === "Won't") {
-      wontData.push(
-        new Issue(
-          json[i].name,
-          json[i].description,
-          json[i].number,
-          json[i].selected,
-          json[i].moscow,
-          json[i].assignee,
-        ),
-      );
+          "",
+          json[i].assignee
+      ))
     }
   }
 
-  return [availableData, mustData, shouldData, couldData, wontData];
+  return [mustIssues, shouldIssues, couldIssues, wontIssues, todoIssues];
 }
